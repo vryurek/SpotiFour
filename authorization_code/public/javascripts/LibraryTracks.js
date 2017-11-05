bf0a458aae0a34ad5b9cb8a5d96f894306cbcc34
Veronica Yurek <vryurek@mtu.edu>
2017-11-05 01:29:28 -0400

    fixed pause button, added global top 50, US top 50, fixed library tracks display, album display, artist display, library album display

--- authorization_code/public/javascripts/LibraryTracks.js
+++ authorization_code/public/javascripts/LibraryTracks.js
@@ -1,19 +1,32 @@
 /**
- *This file displays all the tracks in the user's Music Library
+ *This file displays up to 250 tracks in the user's Music Library
  **/
 var access_token = localStorage.getItem('access');
+var num = 0;    //number of tracks
 
-$.ajax({    //get track info
-    url: 'https://api.spotify.com/v1/me/tracks',
-    headers: {
-        'Authorization': 'Bearer ' + access_token
-    },
-    success: function (tracks) {
-        document.getElementById('headinfo').innerText = tracks.items.length + " songs";
-        var trackList = new Array(0);
-        for (var i = 0; i < tracks.items.length; i++) {
-            trackList.push(tracks.items[i].track);
-        }
-        displayTracks(trackList);
-    }
-});
+//max amount of tracks you can query from spotify is 50, so loop
+//in order to get more than 50.
+for (var j = 0; j < 5; j++) {
+    (function () {
+        $.ajax({    //get track info
+            url: 'https://api.spotify.com/v1/me/tracks?offset=' + (j * 50) + '&limit=50',
+            headers: {
+                'Authorization': 'Bearer ' + access_token
+            },
+            success: function (tracks) {
+                num += tracks.items.length;
+                document.getElementById('headinfo').innerText = num + " songs";
+
+                //get array of track objects from array of saved track objects
+                var trackList = new Array(0);
+                for (var i = 0; i < tracks.items.length; i++) {
+                    trackList.push(tracks.items[i].track);
+                }
+                displayTracks(trackList);   //display track list
+
+            }
+        });
+    })()
+
+}
+
--- authorization_code/public/javascripts/playlist.js
+++ authorization_code/public/javascripts/playlist.js
@@ -1,4 +1,7 @@
-/*This file displays the tracks from a playlist*/
+
+/**
+ * This file displays up to 100 tracks from a playlist
+ * */
 
 var access_token = localStorage.getItem('access');      //retrieve the access token
 
@@ -8,18 +11,11 @@ var access_token = localStorage.getItem('access');      //retrieve the access to
 function getParams() {
     var url = document.location.href;
     var paramList = url.split('?')[1].split('&');
-    var userID = paramList[0].split('=')[1];
-    var playlistID = paramList[1].split('=')[1];
-    var playlistName = paramList[2].split('=')[1];
+    var userID = paramList[0].split('=')[1];    //the user's id
+    var playlistID = paramList[1].split('=')[1];    //the playlist's id
+    var playlistName = decodeURIComponent(paramList[2].split('=')[1]);  //the name of the playlist
 
-    //If the playlist name has a space, it will display '%20' by default. This fixes it.
-    var name ="";
-    var words = playlistName.split('%20');
-    for (var k = 0; k < words.length; k++) {
-        name += words[k] + " ";
-    }
-
-    document.getElementById('head').innerText = name;   //write playlist name to the head text
+    document.getElementById('head').innerText = playlistName;   //write playlist name to the head text
 
     //the path to get tracks from a playlist
     var trackURL = 'https://api.spotify.com/v1/users/' + userID + '/playlists/' + playlistID + '/tracks';
@@ -29,6 +25,7 @@ function getParams() {
 
 /**
  * retrieve and show the tracks in a playlist
+ * @param url the url endpoint
  */
 function showTracks(url) {
     if (url === null) { //return if the url is null
